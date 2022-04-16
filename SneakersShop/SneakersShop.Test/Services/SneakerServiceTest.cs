490301d213c92ba09942ca546fdba282a1ba6ca8
kaloyancvetkov <kaloyancvetkov21@gmail.com>
2022-04-16 17:36:42 +0300

    added testing

--- SneakersShop/SneakersShop.Test/Services/SneakerServiceTest.cs
+++ SneakersShop/SneakersShop.Test/Services/SneakerServiceTest.cs
@@ -39,10 +39,63 @@ namespace SneakersShop.Test.Services
         }
 
         [Test]
-        public void AllCategoriesShouldReturnValidData()
+        public void IsBySellerShouldWorkProperly()
         {
             //Arrange
             using var data = DatabaseMock.Instance;
+
+
+            var seller = new Seller { Id = 1, UserId = "1", Name = "Pesho", PhoneNumber = "052644" };
+            var sneaker = new Sneaker
+            {
+                Id = 33,
+                Brand = "Nike",
+                Model = "Air Max 2090",
+                Color = "Yellow",
+                Description = "just something",
+                ImageUrl = "blablablabla",
+                Price = 250,
+                Seller = seller
+            };
+            data.Sneakers.Add(sneaker);
+            data.SaveChanges();
+
+            var sneakerService = new SneakerService(data, null, null);
+
+            //Act
+            var result = sneakerService.IsBySeller(sneaker.Id, seller.Id);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CategoryExistsShouldWorkProperly()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+
+            var category = new Category { Id = 45, Name = "Lifestyle" };
+            data.Categories.Add(category);
+            data.SaveChanges();
+
+            var sneakerService = new SneakerService(data, null, null);
+
+            //Act
+            var result = sneakerService.CategoryExists(category.Id);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CreateShouldReturnCorrectIdWhenCreated()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+
             var sneaker = new Sneaker
             {
                 Brand = "Nike",
@@ -51,10 +104,34 @@ namespace SneakersShop.Test.Services
                 Description = "just something",
                 ImageUrl = "blablablabla",
                 Price = 250,
-                Seller = new Seller { UserId = "1", Name = "Pesho", PhoneNumber = "052644" }
+                CategoryId = 2,
+                SellerId = 4
             };
 
-            data.Sneakers.Add(new Sneaker
+            var sneakerService = new SneakerService(data, null, null);
+
+            //Act
+            var result = sneakerService.Create(sneaker.Brand, 
+                sneaker.Model, 
+                sneaker.Color, 
+                sneaker.Description, 
+                sneaker.ImageUrl, 
+                sneaker.Price, 
+                sneaker.CategoryId, 
+                sneaker.SellerId);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void EditShouldReturnTrueWhenEdited()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+
+            var sneaker = new Sneaker
             {
                 Brand = "Nike",
                 Model = "Air Max 2090",
@@ -62,18 +139,29 @@ namespace SneakersShop.Test.Services
                 Description = "just something",
                 ImageUrl = "blablablabla",
                 Price = 250,
-                Seller = new Seller { UserId = "1", Name = "Pesho", PhoneNumber = "052644"}
-            });
+                CategoryId = 2,
+                SellerId = 4
+            };
+            data.Sneakers.Add(sneaker);
             data.SaveChanges();
 
             var sneakerService = new SneakerService(data, null, null);
 
             //Act
-            var result = sneakerService.ByUser("1");
+            var result = sneakerService.Edit(
+                1,
+                "Adidas",
+                "Yeezy",
+                "Blue",
+                "Some long descr",
+                "Some link from net",
+                330,
+                4,
+                true);
 
             //Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(sneaker, result);
+            Assert.IsTrue(result);
         }
     }
 }
